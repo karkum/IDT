3d0f14937eb70c4d958064433ec8d8c519b05a53
marknach <marknach@vt.edu>
2012-03-25 15:12:10 -0300

    Update src/IDT.java

--- src/IDT.java
+++ src/IDT.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
 import java.io.File;
@@ -11,17 +12,25 @@ public class IDT {
 	public static void main(String [] args) throws IOException {
 		Scanner in = new Scanner(System.in);
 		BufferedImage img = ImageIO.read(new File(in.nextLine()));
+		BufferedImage newimg = new BufferedImage(img.getWidth(),img.getHeight(),BufferedImage.TYPE_INT_RGB );
 		Raster ras = img.getData();
-		for (int i = 0; i < img.getHeight(); i++) {
-			for (int j = 0; j < img.getWidth(); j++) {
+		for (int i = 1; i < img.getHeight() - 1; i++) {
+			for (int j = 1; j < img.getWidth() - 1; j++) {
 				int rgb = img.getRGB(j, i);
-				if (rgb == -1)
-					System.out.print(0);
+				int next = img.getRGB(j, i+1);
+				int prev = img.getRGB(j, i-1);
+				int above = img.getRGB(j+1, i);
+				int below = img.getRGB(j-1, i);
+				if ( Math.abs(rgb - next) > 5 &&  Math.abs(rgb - prev) > 5 )
+				    newimg.setRGB(j, i, Color.black.getRGB());
+				else if ( Math.abs(rgb - above) > 5 &&  Math.abs(rgb - below) > 5 ) 
+				    newimg.setRGB(j, i, Color.black.getRGB());
 				else
-					System.out.print(1);
+				    newimg.setRGB(j, i, Color.white.getRGB());
 			}
-			System.out.println();
 		}
+		File file = new File("newimg.png");
+		ImageIO.write(newimg,"png",file);
 	}
 }
  	
